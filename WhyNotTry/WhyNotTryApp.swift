0568e1fe375a0b4aa56875df2f44e194600e5195
HJ <jm30806@naver.com>
2025-02-04 00:02:37 +0900

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by 혜주가짱 on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhyNotTryTests/WhyNotTryTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryTests.swift
+//  WhyNotTryTests
+//
+//  Created by 혜주가짱 on 2/4/25.
+//
+
+import Testing
+@testable import WhyNotTry
+
+struct WhyNotTryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
