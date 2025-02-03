e19d4e30d372c342ab0a4ba28e113a5fdfb8a651
HJ <jm30806@naver.com>
2025-02-04 00:14:34 +0900

    including image

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Circle()
+            .fill(.blue)
+            .padding()
+            .overlay(
+                Image(systemName: "figure.archery")
+                    .font(.system(size: 144))
+                    .foregroundStyle(.white)
+            )
     }
 }
 
